62ad4089b10441e7b0bffbab7540699ac5098dcc
Zuheng <zuhengdavid@gmail.com>
2024-01-14 23:01:24 -0800

    minor update vim-slime

--- lua/zuhengdav/plugins/vim-slime.lua
+++ lua/zuhengdav/plugins/vim-slime.lua
@@ -1,9 +1,13 @@
 return {
 	"jpalardy/vim-slime",
 	ft = { "julia", "python", "r" },
+	-- init = function()
+	-- 	vim.g.slime_no_mappings = 1 -- disable default mappings
+	-- end,
 	config = function()
 		vim.g.slime_target = "tmux"
 		vim.g.slime_default_config = { socket_name = "default", target_pane = "{last}" }
+
 		-- vim.g.slime_default_config = {
 		-- 	-- Lua doesn't have a string split function!
 		-- 	socket_name = vim.api.nvim_eval('get(split($TMUX, ","), 0)'),
