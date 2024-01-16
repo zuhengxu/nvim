1287436f4fd971ca39301913be9f3df75e449cfc
Zuheng <zuhengdavid@gmail.com>
2024-01-15 16:06:28 -0800

    make markdown-preview work

--- lua/zuhengdav/plugins/markdown.lua
+++ lua/zuhengdav/plugins/markdown.lua
@@ -16,21 +16,10 @@ return {
 	{
 		"iamcco/markdown-preview.nvim",
 		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-		build = "cd app && npm install",
+		build = "cd app && yarn install",
 		init = function()
 			vim.g.mkdp_filetypes = { "markdown" }
 		end,
 		ft = { "markdown" },
-		config = function()
-			vim.g.mkdp_filetypes = { "markdown" }
-			vim.g.mkdp_auto_close = 0
-
-			-- Set up an autocmd event to apply the key mapping for markdown files
-			vim.cmd([[
-		      autocmd FileType markdown nnoremap <buffer> <leader>ll :MarkdownPreview<CR>
-		      autocmd FileType markdown nnoremap <buffer> <leader>lc :MarkdownPreviewStep<CR>
-		      autocmd FileType markdown nnoremap <buffer> <leader>lt :MarkdownPreviewToggle<CR>
-			    ]])
-		end,
 	},
 }
