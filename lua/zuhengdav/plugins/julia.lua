faa61fe1a40d3ffc4d43ce7485fd1f8db1354c26
Zuheng <zuhengdavid@gmail.com>
2024-01-20 16:40:50 -0800

    add julia formtter vim

--- lua/zuhengdav/plugins/julia.lua
+++ lua/zuhengdav/plugins/julia.lua
@@ -10,4 +10,26 @@ return {
 	{
 		"andreypopp/julia-repl-vim",
 	},
+	{
+		--formatter
+		"kdheepak/JuliaFormatter.vim",
+		lazy = false,
+		filetypes = { "julia" },
+		config = function()
+			-- Normal mode mapping
+			vim.api.nvim_set_keymap(
+				"n",
+				"<localleader>ff",
+				":JuliaFormatterFormat<CR>",
+				{ noremap = true, silent = true }
+			)
+			-- Visual mode mapping
+			vim.api.nvim_set_keymap(
+				"v",
+				"<localleader>ff",
+				":JuliaFormatterFormat<CR>",
+				{ noremap = true, silent = true }
+			)
+		end,
+	},
 }
