7ba1f37a09627ea118f137770e4cf9ae58a32338
Zuheng <zuhengdavid@gmail.com>
2024-01-24 21:59:29 -0800

    add todo-comment/trouble

---
+++ lua/zuhengdav/plugins/todo.lua
@@ -0,0 +1,22 @@
+return {
+	{
+		"folke/todo-comments.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		opts = {
+			-- your configuration comes here
+			-- or leave it empty to use the default settings
+			-- refer to the configuration section below
+		},
+	},
+
+	{
+		"folke/trouble.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		opts = {
+			-- your configuration comes here
+			-- or leave it empty to use the default settings
+			-- refer to the configuration section below
+			-- TODO: is this even helpful
+		},
+	},
+}
