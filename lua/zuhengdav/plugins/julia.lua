2aa6531c918dd8766bfdc570a10e2eb5672b116d
Zuheng <zuhengdavid@gmail.com>
2024-01-24 21:32:09 -0800

    turn tab tex to unicode on

--- lua/zuhengdav/plugins/julia.lua
+++ lua/zuhengdav/plugins/julia.lua
@@ -3,8 +3,8 @@ return {
 		"JuliaEditorSupport/julia-vim",
 		lazy = false,
 		config = function()
-			vim.g.latex_to_unicode_tab = "off"
-			vim.g.latex_to_unicode_auto = 1
+			vim.g.latex_to_unicode_tab = "on"
+			vim.g.latex_to_unicode_auto = 0
 		end,
 	},
 	{
