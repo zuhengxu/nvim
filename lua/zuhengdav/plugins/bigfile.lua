45c83be6621be41c7ebf209fd0b420d4cb03650d
Zuheng <zuhengdavid@gmail.com>
2024-02-01 17:25:25 -0800

    add bigfile.nvim

---
+++ lua/zuhengdav/plugins/bigfile.lua
@@ -0,0 +1,20 @@
+return {
+	"LunarVim/bigfile.nvim",
+	event = "BufReadPre",
+	opts = {
+		filesize = 2, -- size of the file in MiB, the plugin round file sizes to the closest MiB
+		features = { -- features to disable
+			"indent_blankline",
+			"illuminate",
+			"lsp",
+			"treesitter",
+			"syntax",
+			"matchparen",
+			"vimopts",
+			"filetype",
+		},
+	},
+	config = function(_, opts)
+		require("bigfile").setup(opts)
+	end,
+}
