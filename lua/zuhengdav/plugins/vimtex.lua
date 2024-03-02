ddb7aa34fb19ddf0f613987c0b38ed64833b2301
Zuheng <zuhengdavid@gmail.com>
2024-03-02 10:20:59 -0800

    set latex compiler latexmk

--- lua/zuhengdav/plugins/vimtex.lua
+++ lua/zuhengdav/plugins/vimtex.lua
@@ -3,7 +3,7 @@ return {
 	config = function()
 		vim.g.vimtex_view_method = "zathura"
 		vim.g.vimtex_view_general_viewer = "zathura"
-		-- vim.g.vimtex_compiler_progname = "nvr"
+		vim.g.vimtex_compiler_progname = "latexmk"
 		-- vim.g.maplocalleader = " " --configured localleader in set.lua
 	end,
 }
