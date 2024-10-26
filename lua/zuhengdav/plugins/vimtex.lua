a580397842a4b16cc8f77cd092fe0c5d7eb1ba88
Zuheng <zuhengdavid@gmail.com>
2024-10-26 15:30:31 -0700

    remove autoformat on save & change latexcomp to xelatex

--- lua/zuhengdav/plugins/vimtex.lua
+++ lua/zuhengdav/plugins/vimtex.lua
@@ -4,6 +4,7 @@ return {
 		vim.g.vimtex_view_method = "zathura"
 		vim.g.vimtex_view_general_viewer = "zathura"
 		vim.g.vimtex_compiler_progname = "latexmk"
-		-- vim.g.maplocalleader = " " --configured localleader in set.lua
+        vim.g.vimtex_compiler_latexmk_engines = { ["_"]= '-xelatex' }
+        -- vim.g.maplocalleader = " " --configured localleader in set.lua
 	end,
 }
