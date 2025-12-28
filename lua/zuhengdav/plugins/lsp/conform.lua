e42f55f9683b6e2083ba0488f293dcaae2941c27
Zuheng <zuhengdavid@gmail.com>
2025-12-27 17:03:14 -0800

    update conform python config

--- lua/zuhengdav/plugins/lsp/conform.lua
+++ lua/zuhengdav/plugins/lsp/conform.lua
@@ -34,14 +34,7 @@ return {
 			formatters_by_ft = {
 				lua = { "stylua" },
 				-- Conform can also run multiple formatters sequentially
-				python = function(bufnr)
-					if require("conform").get_formatter_info("ruff_format", bufnr).available then
-						return { "ruff_format" }
-					else
-						return { "isort", "black" }
-					end
-				end,
-
+				python = {"ruff", "ruff_organize_imports", "ruff_format"},
 				--
 				-- You can use 'stop_after_first' to run the first available formatter from the list
 				-- javascript = { "prettierd", "prettier", stop_after_first = true },
